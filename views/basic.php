2ad1389c56e472203cbe117389dc658e79b53549
Raj Mangewala <raji_mangewala@yahoo.com>
2011-11-27 11:22:28 -0800

    Moved script tags to bottom of body and added non-blocking dynamic script generation.

--- views/basic.php
+++ views/basic.php
@@ -8,11 +8,8 @@
 <title><?php echo $pgtitle; ?></title>
 <link href="<?php echo __SITE_ROOT; ?>css/style1.css"  rel="stylesheet" 
 						   type="text/css" /> 
-<!-- JavaScripts -->
-<script type="text/javascript"
-      src="http://code.jquery.com/jquery-1.7.min.js"></script>
-<script type="text/javascript" 
-	src="<?php echo __SITE_ROOT; ?>js/ajax.js"></script>
+<!--script type="text/javascript" 
+	src="<?php //echo __SITE_ROOT; ?>js/ajax.js"></script-->
 <!-- END Javascripts -->
 </head>
 <body>
@@ -23,5 +20,39 @@
 <p>Alexa Code Sample Search</p>
 </div> <!-- footer -->
 <img id="sample" src="<?php echo __SITE_ROOT;?>images/sample_search_result.jpg" alt="sample view" />
+<!-- JavaScripts -->
+<script type="text/javascript"
+      src="http://code.jquery.com/jquery-1.7.min.js"></script>
+<script type="text/javascript">
+//alert('test0');
+    function loadScript(url, callback){
+
+        var script = document.createElement("script")
+        script.type = "text/javascript";
+
+        if (script.readyState){  //IE
+            script.onreadystatechange = function(){
+                if (script.readyState == "loaded" ||
+                        script.readyState == "complete"){
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else {  //Others
+            script.onload = function(){
+                callback();
+            };
+        }
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+
+    loadScript("<?php echo __SITE_ROOT; ?>js/ajax.js", function(){
+        App.init();
+        App.events();
+    });
+</script>
+
 </body>
 </html>
