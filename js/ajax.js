85b699d49d715c44ead643255861186c37a0ffb9
Raj Mangewala <raji_mangewala@yahoo.com>
2011-12-07 11:50:03 -0800

    Moved History.js init function here from basic.php.  Added pup sub functionality to decouple application logic from ajax calls.

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,105 @@
-var App = {}
+ var App = {}
+
+/////////////////////////////////////////////////////////////////////////
+//		Plugin Modules
+//
+//  1)Pub Sub - Decouple Application logic from ajax calls
+//  2)History.js - jQuery Back Button Support
+//
+//
 
 
+/**********************************************************************/
+/* 1.
+/* Library Agnostic Pubsub - v1.0
+/* Copyright 2010
+/* Darcy Clarke http://darcyclarke.me
+/*
+/**********************************************************************/
+ 
+ App.cache = {};
+
+ App.publish = function(topic, args){
+
+    App.cache[topic] && $.each(App.cache[topic], function(){
+	this.apply($, args || []);
+    });
+ };
+
+ App.subscribe = function(topic, callback){
+    if(!App.cache[topic]){
+	App.cache[topic] = [];
+    }
+    App.cache[topic].push(callback);
+    return [topic, callback];
+ };
+
+ App.unsubscribe = function(handle){
+    var t = handle[0];
+    App.cache[t] && $.each(App.cache[t], function(idx){
+	if(this == handle[1]){
+	    App.cache[t].splice(idx, 1);
+	}
+    });
+ };
+
+/**********************************************************************/
+/* 2.	github(dot)com/balupton/History.js/
+/* 
+/* Copyright (c) 2011, Benjamin Arthur Lupton
+/* All rights reserved.
+/* 
+/* Redistribution and use in source and binary forms, with or without 
+/* modification, are permitted provided that the following conditions 
+/* are met:
+/* 
+/* Redistributions of source code must retain the above copyright notice, 
+/* this list of conditions and the following disclaimer.
+/* Redistributions in binary form must reproduce the above copyright notice,
+/* this list of conditions and the following disclaimer in the documentation
+/* and/or other materials provided with the distribution.
+/* Neither the name of Benjamin Arthur Lupton nor the names of its 
+/* contributors may be used to endorse or promote products derived from 
+/* this software without specific prior written permission.
+/* 
+/* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+/* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+/* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+/* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+/* COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+/* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+/* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+/* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+/* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+/* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+/* ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+/* POSSIBILITY OF SUCH DAMAGE.
+/*  
+/**********************************************************************/
+
+ App.history = function(window,undefined){
+
+    // Prepare
+    var History = window.History; // Note: We are using a capital H instead of a lower h
+    if ( !History.enabled ) {
+         // History.js is disabled for this browser.
+         // This is because we can optionally choose to support HTML4 browsers or not.
+        return false;
+    }
+
+    // Bind to StateChange Event
+    History.Adapter.bind(window,'statechange',function(){ //  using statechange instead of popstate
+        var State = History.getState(); //  using History.getState() instead of event.state
+        History.log(State.data, State.title, State.url);
+    });
+
+ };
+
+/////////////////////////////////////////////////////////////////////////
+//		Application Specific Logic
+//
+//
+  
  App.init = function () {
 
     /* place cursor in text search */
@@ -14,8 +113,22 @@ var App = {}
 
     var $form   = $('#searchform'),
         srchTxt = $form.find( 'input[name="searchtext"]' ),
-	title   = document.title;
+	title   = document.title || null;
 
+    //Subscribe(bind event to callback)
+    App.subscribe("statechange", function(data, type) {
+	if (type == 'none') {
+	    $('#searchContent').html('');
+    	    srchTxt.val('')
+	    App.init();
+	} else {
+	    var content = $(data).find('#searchResults'); 
+	    var termCln = $(data).find('input[name="searchtext"]').val(); //server sanitize
+	    $('#searchContent').html(content);
+    	    srchTxt.val(termCln)
+	    App.init();
+	}
+    });
 
     /* setup History actions */
     $(window).bind('statechange',function(){
@@ -37,21 +150,17 @@ var App = {}
     	    	page   = params['p'];
 	    }
     
+	    //encodeURIComponent does not work with History.js which decodes 
     	    baseUrl = stUrl.split('results')[0]+'results?q='+encodeURIComponent(term)+'&p='+page;
 
     	    $.post( baseUrl, { searchtext: term },
 	     	function(data) {
-		    var content = $(data).find('#searchResults'); 
-		    var termCln = $(data).find('input[name="searchtext"]').val(); //server sanitize
-		    $('#searchContent').html(content);
-    		    srchTxt.val(termCln)
-		    App.init();
-	
+		    //Publish(trigger event)
+		    App.publish("statechange", [data, 'post']); //array required
 	    });
+
     	} else {
-	    $('#searchContent').html('');
-    	    srchTxt.val('')
-	    App.init();
+	    App.publish("statechange", ['', 'none']);
     	}
     });
 
@@ -107,7 +216,12 @@ var App = {}
     }
  }
     
- /* taken from stackoverflow(dot)com/questions/901115/get-query-string-values-in-javascript */
+/////////////////////////////////////////////////////////////////////////
+//		Helper Functions
+//
+  
+// stackoverflow(dot)com/questions/901115/get-query-string-values-in-javascript
+  
  App.urlParse = function (queryString) {
 
     var urlParams = {},
@@ -116,7 +230,7 @@ var App = {}
         r = /([^&=]+)=?([^&]*)/g,
         d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
         //q = window.location.search.substring(1);
-        q = queryString;
+        q = queryString.replace('%', '%25');
 
     while (e = r.exec(q))
        urlParams[d(e[1])] = d(e[2]);
