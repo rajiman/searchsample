2b264b5e5644b7bb4b409ee780e574bd63c6df90
Raj Mangewala <raji_mangewala@yahoo.com>
2011-12-07 11:48:03 -0800

    Moved History init function to App namespace in ajax.js.  Changed History.js source to local server(was balupton git).

--- views/basic.php
+++ views/basic.php
@@ -23,26 +23,7 @@
 <script type="text/javascript"
       src="http://code.jquery.com/jquery-1.7.min.js"></script>
 <script 
-src="http://balupton.github.com/history.js/scripts/bundled/html4+html5/jquery.history.js"></script>
-<script type="text/javascript">
-(function(window,undefined){
-
-    // Prepare
-    var History = window.History; // Note: We are using a capital H instead of a lower h
-    if ( !History.enabled ) {
-         // History.js is disabled for this browser.
-         // This is because we can optionally choose to support HTML4 browsers or not.
-        return false;
-    }
-
-    // Bind to StateChange Event
-    History.Adapter.bind(window,'statechange',function(){ //  using statechange instead of popstate
-        var State = History.getState(); //  using History.getState() instead of event.state
-        History.log(State.data, State.title, State.url);
-    });
-
-})(window);
-</script>
+src="<?php echo __SITE_ROOT;?>js/history.js/scripts/bundled/html4+html5/jquery.history.js"></script>
 <script type="text/javascript">
     function loadScript(url, callback){
 
@@ -68,6 +49,7 @@ src="http://balupton.github.com/history.js/scripts/bundled/html4+html5/jquery.hi
     }
 
     loadScript("<?php echo __SITE_ROOT; ?>js/ajax.js", function(){
+        App.history(window, undefined);
         App.init();
         App.events();
 	App.preload("<?php echo __SITE_ROOT; ?>images/link-icon.png");
